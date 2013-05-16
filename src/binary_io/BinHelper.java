faa079cdd95d32b3f90e85d4e766d622c72ba020
Joffrey BION <joffrey.bion@gmail.com>
2013-05-16 15:06:45 -0400

    Use a StringBuilder to append zeros in front a string in BinHelper.

--- src/binary_io/BinHelper.java
+++ src/binary_io/BinHelper.java
@@ -3,11 +3,12 @@ package binary_io;
 public class BinHelper {
 
     public static String addLeadingZeros(String str, int length) {
-        int nbMissingZeros = length - str.length();
-        for (int i = 0; i < nbMissingZeros; i++) {
-            str = "0" + str;
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < length - str.length(); i++) {
+            sb.append("0");
         }
-        return str;
+        sb.append(str);
+        return sb.toString();
     }
     
 }
