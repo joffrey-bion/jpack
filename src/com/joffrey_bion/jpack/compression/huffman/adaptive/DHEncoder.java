5d437d39d9a097df4b310282c807be14f47632dc
Joffrey BION <joffrey.bion@gmail.com>
2013-06-22 20:01:56 -0400

    Merge with old files

---
+++ src/com/joffrey_bion/jpack/compression/huffman/adaptive/DHEncoder.java
@@ -0,0 +1,24 @@
+package com.joffrey_bion.jpack.compression.huffman.adaptive;
+
+import java.io.InputStreamReader;
+
+public class DHEncoder {
+
+    InputStreamReader input;
+    
+    public DHEncoder(InputStreamReader isr) {
+        this.input = isr;
+        // TODO new empty tree
+    }
+
+    public void encode(Character c) {
+        // TODO write code
+        // TODO insert c
+    }
+
+    public Character decode() {
+        // TODO read code and decode char or NYT+char
+        // TODO insert char in the Tree
+        return null;
+    }
+}
